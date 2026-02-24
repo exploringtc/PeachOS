43dca7e575be4e151b38b514491fc3f45b5914b6
ThomasCho <tcho95@gmail.com>
2026-02-23 22:00:38 -0500

    Initial commit: PeachOS kernel implementation

---
+++ src/fs/file.h
@@ -0,0 +1,63 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "pparser.h"
+
+typedef unsigned int FILE_SEEK_MODE;
+enum {
+    SEEK_SET,
+    SEEK_CUR,
+    SEEK_END
+};
+
+typedef unsigned int FILE_MODE;
+enum {
+    FILE_MODE_READ,
+    FILE_MODE_WRITE,
+    FILE_MODE_APPEND,
+    FILE_MODE_INVALID
+};
+
+struct disk;
+
+// These are function pointers to be inherited by a particular filesystem driver.
+typedef void* (*FS_OPEN_FUNCTION)(struct disk* disk, struct path_part* path, FILE_MODE mode);
+typedef int (*FS_RESOLVE_FUNCTION)(struct disk* disk);
+
+struct filesystem{
+    // The filesystem should return zero from resolve if the provided disk is using its filesystem
+    // When the VFS layer wants to determine a filesystem, it will loop through all available registered
+    // filesystems and run resolve. if it returns zero, then the filesystem driver is indicating that
+    // it understands how to read the given disk.
+    FS_RESOLVE_FUNCTION resolve;
+    
+    //this function pointer is called when we opened a file in a given filesystem.
+    FS_OPEN_FUNCTION open;
+
+    // This is the name of the filesystem, for example "FAT16".
+
+    char name[20];
+};
+
+//File descriptors represent open files. when you call "fopen",
+// a file is opened and a file descriptor is created to represent it.
+struct file_descriptor {
+    // the descriptor index.
+    int index;
+
+    // the filesystem that this particular file is in
+    struct filesystem* filesystem;
+
+    // Private data for internal file descriptor
+    void* private;
+
+    // The disk that the file descriptor should be used on.
+    struct disk* disk;
+};
+
+void fs_init();
+int fopen(const char* filename, const char* module);
+void fs_insert_filesystem(struct filesystem* filesystem);
+struct filesystem* fs_resolve(struct disk* disk);
+
+#endif
---
+++ src/kernel.h
@@ -0,0 +1,12 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+
+#define PEACHOS_MAX_PATH 108
+
+void kernel_main();
+void print(const char* str);
+
+#endif
